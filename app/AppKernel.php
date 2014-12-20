58b1a991a87a7ac0fae873b1bb185131f8ca0e4a
Tomek <n1kula@wp.pl>
2014-12-20 15:40:52 +0100

    data fixtures installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Jobeet\Bundle\BackendBundle\JobeetBackendBundle(),
             new Jobeet\Bundle\FrontendBundle\JobeetFrontendBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
