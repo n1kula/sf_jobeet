98626cebeef1d6195a1545765f7a75e14ef3186e
Tomek <n1kula@wp.pl>
2014-12-20 14:39:58 +0100

    layout+backend bundles installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Jobeet\Bundle\BackendBundle\JobeetBackendBundle(),
+            new Jobeet\Bundle\FrontendBundle\JobeetFrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
